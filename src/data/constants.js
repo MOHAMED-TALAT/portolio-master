37cf43032112a69f1e7e087480bc33b21be9959b
MOHAMED-TALAT <89748812+MOHAMED-TALAT@users.noreply.github.com>
2023-08-26 21:30:44 +0300

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -9,7 +9,7 @@ export const Bio = {
   description:
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/MOHAMED-TALAT",
-  resume: "",
+  resume: "https://drive.google.com/file/d/1t-nN0R7TXtWMG-ztN7eNTnwtGEf6GAL6/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/mohamed-talaat-837111217/",
   // twitter: "https://twitter.com/RishavChanda",
   // insta: "#",
